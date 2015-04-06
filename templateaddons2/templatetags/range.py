833c4b9caaf4fa4fd5cc766aaa4670a29a62ab3a
Max Vyaznikov <maxvyaznikov@gmail.com>
2015-04-06 12:11:52 +0300

    fix step in range tag

--- templateaddons2/templatetags/range.py
+++ templateaddons2/templatetags/range.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 @register.assignment_tag(name='range')
-def range_tag(start=None, stop=None, step=None):
+def range_tag(start=None, stop=None, step=1):
     """
     {% range [start,] stop[, step] as context_name %}
     """
@@ -11,8 +11,10 @@ def range_tag(start=None, stop=None, step=None):
         # {% range 5 as r %} => range(start=0, stop=5, step=1)
         stop = start
         start = 0
-    return range(start if start is not None else 0, stop, step if step is not None else 1)
-
+    if stop is not None:
+        return range(start if start is not None else 0, stop, step)
+    else:
+        return ''
 
 @register.filter()
 def times(value):
